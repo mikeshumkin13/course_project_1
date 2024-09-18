b742113208d63296046114df978ce0bb7457f4f9
a12345 <shu.m2010@yandex.ru>
2024-09-18 15:31:19 +0300

    add test_reports.py

---
+++ tests/test_reports.py
@@ -0,0 +1,7 @@
+import pytest
+from src.reports import generate_report
+
+def test_generate_report():
+    report = generate_report('data/operations.xlsx')
+    assert isinstance(report, pd.DataFrame)  # Проверяем, что результат — это DataFrame
+    assert not report.empty  # Проверяем, что отчет не пустой
